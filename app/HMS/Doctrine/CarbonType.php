baeebdb9f9ac9032d553b342835e1acf146621ae
James Hodgson <james.hodgson@nottingham.ac.uk>
2016-10-24 18:54:40 +0100

    Added carbon type to override DateTime type
    Will now return Carbon objects from DB instead of DateTime objects

---
+++ app/HMS/Doctrine/CarbonType.php
@@ -0,0 +1,21 @@
+<?php
+namespace HMS\Doctrine;
+
+
+use Carbon\Carbon;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class CarbonType extends DateTimeType
+{
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        $result = parent::convertToPHPValue($value, $platform);
+
+        if ($result instanceof \DateTime) {
+            return Carbon::instance($result);
+        }
+        return $result;
+    }
+}

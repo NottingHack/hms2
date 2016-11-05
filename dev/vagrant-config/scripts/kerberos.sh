0f59fc76a42f32f5ef842bf222b4ca643ec88b1d
dpslwk <dps.lwk@gmail.com>
2016-11-05 13:03:12 +0000

    Vagrant: shut up apt-get in Kerberos install

--- dev/vagrant-config/scripts/kerberos.sh
+++ dev/vagrant-config/scripts/kerberos.sh
@@ -17,7 +17,7 @@ debconf-set-selections <<< 'krb5-config krb5-config/admin_server string hmsdev.n
 
 
 # Install krb, create passord database, and set the master password to "krbMasterPassword"
-apt-get install php-pear php7.0-dev libkrb5-dev haveged krb5-{admin-server,kdc} -y
+apt-get install php-pear php7.0-dev libkrb5-dev haveged krb5-{admin-server,kdc} -y > /dev/null 2>&1
 kdb5_util create -s -P krbMasterPassword
 mkdir /var/log/kerberos
 touch /var/log/kerberos/{krb5kdc,kadmin,krb5lib}.log

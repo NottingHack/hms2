a27cbb27eafd78ac56e226c697ec2867c97ca9eb
dpslwk <dps.lwk@gmail.com>
2016-11-27 19:06:51 +0000

    Register Interest: Adding invites:purge command and running daily

--- app/HMS/Repositories/InviteRepository.php
+++ app/HMS/Repositories/InviteRepository.php
@@ -2,9 +2,11 @@
 
 namespace HMS\Repositories;
 
+use Carbon\Carbon;
 use Hms\Entities\Invite;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class InviteRepository extends EntityRepository
 {
@@ -25,6 +27,24 @@ class InviteRepository extends EntityRepository
             $this->_em->flush();
         }
         return $invite;
+    }
+
+    /**
+     * remove all invites older than a given date
+     * @param  Carbon $date
+     * @return array
+     */
+    public function removeAllOlderThan(Carbon $date)
+    {
+        $criteria = new Criteria();
+        $criteria->where($criteria->expr()->lt('createdAt', $date));
+        // var_dump($criteria);
+        $invites = $this->matching($criteria)->toArray();
+
+        foreach ($invites as $invite) {
+            $this->_em->remove($invite);
+        }
+        $this->_em->flush();
     }
 
 }

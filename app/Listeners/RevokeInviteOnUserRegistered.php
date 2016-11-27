d489e41c39062aee1b523d47ffb0d1d86ff556b7
dpslwk <dps.lwk@gmail.com>
2016-11-27 15:58:41 +0000

    Register Interest: add listener to revoke InviteToken

---
+++ app/Listeners/RevokeInviteOnUserRegistered.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use HMS\Repositories\InviteRepository;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RevokeInviteOnUserRegistered
+{
+    protected $inviteRepository;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(InviteRepository $inviteRepository)
+    {
+        //
+        $this->inviteRepository = $inviteRepository;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $invite = $this->inviteRepository->findOneByEmail($event->user->getEmail());
+
+        \EntityManager::remove($invite);
+        \EntityManager::flush();
+    }
+}

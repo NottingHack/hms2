a0828357048c78ac3ca7f65cd9846d369b3257d9
dpslwk <dps.lwk@gmail.com>
2016-12-27 17:37:47 +0000

    Vagrant: bunch of script clean up’s Include switch to native yarn install

--- dev/vagrant-config/scripts/bootstrap.sh
+++ dev/vagrant-config/scripts/bootstrap.sh
@@ -6,16 +6,16 @@ echo " "
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update  > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
 
-apt-get install -y software-properties-common  vim > /dev/null 2>&1
+apt-get install -y software-properties-common vim git curl > /dev/null 2>&1
 
 # dotDeb
 add-apt-repository -y "deb http://packages.dotdeb.org jessie all"
 wget --progress=bar:force https://www.dotdeb.org/dotdeb.gpg 
 apt-key add dotdeb.gpg
 
-apt-get update  > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
 
 if ! [ -L /srv/www ]; then
   rm -rf /srv/www

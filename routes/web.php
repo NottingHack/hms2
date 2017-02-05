23b86659c33ded071715c7a4ea6e902393779e68
dpslwk <dps.lwk@gmail.com>
2017-02-05 16:44:45 +0000

    AccessCodes: Hyphen route

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::post('register', 'Auth\RegisterController@register');
 
 Route::get('home', 'HomeController@index')->name('home');
 
-Route::get('accessCodes', 'HomeController@accessCodes');
+Route::get('access-codes', 'HomeController@accessCodes')->name('accessCodes');
 
 // Routes in the following group can only be access from inside the hackspace (as defined by the ip range in .env)
 Route::group(['middleware' => 'ipcheck'], function () {

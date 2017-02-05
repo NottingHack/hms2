bbe8cb9551ef879a4f1600528a523b4177ca9160
dpslwk <dps.lwk@gmail.com>
2017-02-05 15:14:25 +0000

    Restore route name on register-interest

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,6 @@ Route::get('home', 'HomeController@index')->name('home');
 
 // Routes in the following group can only be access from inside the hackspace (as defined by the ip range in .env)
 Route::group(['middleware' => 'ipcheck'], function () {
-    Route::get('/register-interest', 'RegisterInterestController@index');
+    Route::get('/register-interest', 'RegisterInterestController@index')->name('registerInterest');
     Route::post('/register-interest', 'RegisterInterestController@registerInterest');
 });

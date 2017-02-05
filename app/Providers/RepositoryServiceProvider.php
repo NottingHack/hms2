fb1cd0b153031a11a30b6a0b7dc0a37b96f86741
dpslwk <dps.lwk@gmail.com>
2017-02-05 23:40:20 +0000

    Repositories: implement ProfileRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -6,15 +6,18 @@ use HMS\Entities\Meta;
 use HMS\Entities\Role;
 use HMS\Entities\User;
 use HMS\Entities\Invite;
+use HMS\Entities\Profile;
 use HMS\Repositories\MetaRepository;
 use HMS\Repositories\RoleRepository;
 use HMS\Repositories\UserRepository;
 use HMS\Repositories\InviteRepository;
+use HMS\Repositories\ProfileRepository;
 use Illuminate\Support\ServiceProvider;
 use HMS\Repositories\Doctrine\DoctrineMetaRepository;
 use HMS\Repositories\Doctrine\DoctrineRoleRepository;
 use HMS\Repositories\Doctrine\DoctrineUserRepository;
 use HMS\Repositories\Doctrine\DoctrineInviteRepository;
+use HMS\Repositories\Doctrine\DoctrineProfileRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -49,6 +52,10 @@ class RepositoryServiceProvider extends ServiceProvider
 
         $this->app->singleton(UserRepository::class, function ($app) {
             return new DoctrineUserRepository($app['em'], $app['em']->getClassMetaData(User::class));
+        });
+
+        $this->app->singleton(ProfileRepository::class, function ($app) {
+            return new DoctrineProfileRepository($app['em'], $app['em']->getClassMetaData(Profile::class));
         });
     }
 }

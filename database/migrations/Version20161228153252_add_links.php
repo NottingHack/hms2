845c4e83a379a264beb4298171260cc357d777f0
dpslwk <dps.lwk@gmail.com>
2017-02-09 09:33:07 +0000

    Links: populate default links

--- database/migrations/Version20161228153252_add_links.php
+++ database/migrations/Version20161228153252_add_links.php
@@ -2,8 +2,8 @@
 
 namespace Database\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\DBAL\Migrations\AbstractMigration;
 
 class Version20161228153252_add_links extends AbstractMigration
 {

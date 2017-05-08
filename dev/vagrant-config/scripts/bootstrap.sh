20e2a537283eb2ef3c77c0bccd94f77179c0401f
dpslwk <dps.lwk@gmail.com>
2017-05-08 11:37:47 +0100

    Vagrant: update to php7.1

--- dev/vagrant-config/scripts/bootstrap.sh
+++ dev/vagrant-config/scripts/bootstrap.sh
@@ -8,7 +8,11 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update > /dev/null 2>&1
 
-apt-get install -y software-properties-common vim git curl > /dev/null 2>&1
+apt-get install -y software-properties-common vim git curl apt-transport-https lsb-release ca-certificates> /dev/null 2>&1
+
+# deb.sury.org
+wget --progress=bar:force  -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 
 # dotDeb
 add-apt-repository -y "deb http://packages.dotdeb.org jessie all"

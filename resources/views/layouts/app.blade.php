143c5fcab3995881e45b884530daae1ba39ae385
dps.lwk <dps.lwk@gmail.com>
2017-05-09 02:01:02 +0100

    JS: need to server the manifest file when using extract

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -144,7 +144,8 @@
 
 
   <!-- Scripts -->
-  <script src="{{ mix('js/vendor.js')}}"></script>
+  <script src="{{ mix('/js/manifest.js') }}"></script>
+  <script src="{{ mix('js/vendor.js') }}"></script>
   <script src="{{ mix('js/app.js') }}"></script>
 
   @stack('scripts')

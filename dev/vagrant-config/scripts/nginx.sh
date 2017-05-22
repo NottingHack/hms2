aa3eb50875aea7aba63f06b7693dfab081ca27c5
dpslwk <dps.lwk@gmail.com>
2017-05-22 07:43:52 +0100

    Vagrant: reconfigure to use our own base box

--- dev/vagrant-config/scripts/nginx.sh
+++ dev/vagrant-config/scripts/nginx.sh
@@ -4,9 +4,7 @@ echo " "
 echo "NGINX"
 echo " "
 
-apt-get install -y nginx-full > /dev/null 2>&1
-mkdir /etc/nginx/ssl
-openssl genrsa -out /etc/nginx/ssl/hmsdev.key 2048 > /dev/null 2>&1
-openssl req -new -x509 -key /etc/nginx/ssl/hmsdev.key -out /etc/nginx/ssl/hmsdev.cert -days 3650 -subj /CN=hmsdev > /dev/null 2>&1
+# now we have the share setup we can enable the site
+ln -s /etc/nginx//sites-available/default /etc/nginx/sites-enabled/default
 
-cp /vagrant/dev/vagrant-config/nginx/default /etc/nginx/sites-available/default
+service nginx restart

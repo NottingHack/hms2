cefcd42ec91c1284a489f4906132c30f1419e8b9
Matthew Walton <matthew@matthew-walton.co.uk>
2017-05-28 15:24:00 +0100

    Links submenu styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,11 +78,11 @@
       <div class="columns small-12 small-order-1 medium-2 medium-order-0 large-2">
 
         @if ( isset($mainNav) )
-            <ul class="menu vertical">
+            <ul class="vertical menu">
             @foreach ($mainNav as $link)
                 <li{!! $link['active'] ? ' class="active"' : '' !!}><a href="{{ $link['url'] }}">{{ $link['text'] }}</a></li>
                 @if ( count($link['links']) > 0 )
-                    <ul>
+                    <ul class="nested vertical menu">
                     @foreach ($link['links'] as $subLink)
                         <li{!! $subLink['active'] ? ' class="active"' : '' !!}><a href="{{ $subLink['url'] }}">{{ $subLink['text'] }}</a></li>
                     @endforeach

b45414c2bca41e882ed80a0a0f1e3c70b8fbdbf1
jlacy23 <jlacy23@icloud.com>
2017-10-23 22:57:05 +0100

    Access Pane had more work done - more responsive to mobile - bootstrap

--- resources/views/pages/access.blade.php
+++ resources/views/pages/access.blade.php
@@ -7,58 +7,69 @@
 
 <div class="container">
   <div class="row">
+      
     <div class="col-sm">
-      <div class="card my-3">
-    <div class="icon-card-body">
+      <div class="card">
+    <div class="card-header icon-card-body">
         <div class="icon-card-icon"><i class="fa fa-key" aria-hidden="true"></i></div>
         <div class="icon-card-content">
             <h3>Door Codes</h3>
+        </div>
+          </div>
+          <div class="card-body">
             <dl>
                 <dt>Street Door</dt>
-                <dd>{{ $outerDoorCode }}</dd>
+                <h4><span class="badge badge-primary">{{ $outerDoorCode }}</span></h4>
+                <hr>
                 <dt>Inner Door</dt>
-                <dd>{{ $innerDoorCode }}</dd>
+                <h4><span class="badge badge-primary">{{ $innerDoorCode }}</span></h4>
             </dl>
         </div>
     </div>
 </div>
-    </div>
-    <div class="col-sm">
-     <div class="card my-3">
-    <div class="icon-card-body">
-        <div class="icon-card-icon">
-            <i class="fa fa-wifi" aria-hidden="true"></i>
-        </div>
+      
+          <div class="col-sm">
+      <div class="card">
+    <div class="card-header icon-card-body">
+        <div class="icon-card-icon"><i class="fa fa-wifi" aria-hidden="true"></i></div>
         <div class="icon-card-content">
             <h3>Member&apos;s WiFi</h3>
+        </div>
+          </div>
+          <div class="card-body">
             <dl>
                 <dt>SSID</dt>
-                <dd>{{ $wifiSsid }}</dd>
-                <dt>PSK</dt>
-                <dd>{{ $wifiPass }}</dd>
+                <h4><span class="badge badge-primary">{{ $wifiSsid }}</span></h4>
+                <hr>
+                <dt>Password (PSK)</dt>
+                <h4><span class="badge badge-primary">{{ $wifiPass }}</span></h4>
             </dl>
         </div>
     </div>
 </div>
-    </div>
-    <div class="col-sm">
-      <div class="card my-3">
-    <div class="icon-card-body">
-        <div class="icon-card-icon">
-            <i class="fa fa-wifi" aria-hidden="true"></i>
-        </div>
+      
+          <div class="col-sm">
+      <div class="card">
+    <div class="card-header icon-card-body">
+        <div class="icon-card-icon"><i class="fa fa-wifi" aria-hidden="true"></i></div>
         <div class="icon-card-content">
             <h3>Guest WiFi</h3>
+        </div>
+          </div>
+          <div class="card-body">
             <dl>
                 <dt>SSID</dt>
-                <dd>{{ $guestWifiSsid }}</dd>
-                <dt>PSK</dt>
-                <dd>{{ $guestWifiPass }}</dd>
+                <h4><span class="badge badge-primary">{{ $guestWifiSsid }}</span></h4>
+                <hr>
+                <dt>Password (PSK)</dt>
+                <h4><span class="badge badge-primary">{{ $guestWifiPass }}</span></h4>
             </dl>
         </div>
     </div>
 </div>
-    </div>
+      
+
+ 
   </div>
 </div>
 

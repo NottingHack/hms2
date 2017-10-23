70bdb92daee7674f549a6f9f8d77a12a2c5df21c
jlacy23 <jlacy23@icloud.com>
2017-10-23 15:38:22 +0100

    Margin added to Cookie button to create space

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,10 +1,10 @@
 <div class="alert alert-primary container-fluid text-center" role="alert">
   <div class="cookie-consent__container">
     <span class="cookie-consent__message">
-        {!! trans('cookieConsent::texts.message') !!}
+        {!! trans('cookieConsent::texts.message') !!} 
     </span>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn btn-primary">
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn btn-primary" style="margin-left: 0.5%">
 	
         {{ trans('cookieConsent::texts.agree') }}
     </button>

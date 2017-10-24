1523f38d283d00eb481f1a7ff36d2f6347863791
jlacy23 <jlacy23@icloud.com>
2017-10-24 13:53:49 +0100

    Update to bootstrap

--- resources/views/labelTemplates/print.blade.php
+++ resources/views/labelTemplates/print.blade.php
@@ -1,55 +1,45 @@
-@extends('layouts.app')
-
-@section('pageTitle', 'Print Label')
-
-@section('content')
+@extends('layouts.app') @section('pageTitle', 'Print Label') @section('content')
 <div class="container">
-<div class="card">
-  <h4 class="card-header"> {{ $templateName}} </h4>
-  <div class="card-body">
-    <pre class="card-text">{{ $template}}</pre>
-    </div>
-    <div class="card-footer">
-    <form role="form" method="POST" action="{{ route('labels.print', $templateName) }}">
-        {{ csrf_field() }}
-        @if ( ! empty($fields))
-        <p>This template requires the following information to print.</p>
-        @foreach ($fields as $field)
-        <div class="form-group">
-          <label for="{{ $field }}" class="form-label">{{ $field }}</label>
-            <input class="form-control" id="{{ $field }}" type="text" name="{{ $field }}" value="{{ old($field, '') }}" required autofocus>
-            @if ($errors->has($field))
-            <p class="help-text">
-              <strong>{{ $errors->first($field) }}</strong>
-            </p>
-            @endif
-          </div>
-        @endforeach
-        @endif
-
-        @if (SiteVisitor::inTheSpace())
-        <p>How many copies would you like.</p>
-        <hr>
-        <div class="form-group">
-          <label for="copiesToPrint" class="form-label">Copies</label>
-            <input class="form-control" style="width: 5%" id="copiesToPrint" type="text" name="copiesToPrint" value="{{ old('copiesToPrint', '1') }}" required autofocus>
-            @if ($errors->has('copiesToPrint'))
-            <p class="help-text">
-              <strong>{{ $errors->first('copiesToPrint') }}</strong>
-            </p>
-            @endif
-          </div>
-        <hr>
-        <div class="card">
-            <button type="submit" class="btn btn-success">Print</button>
-          </div>
-        @else
-        <div class="row">
-            <p>Labels can only be printed from within the space.</p>
+    <div class="card">
+        <h4 class="card-header"> {{ $templateName}} </h4>
+        <div class="card-body">
+            <pre class="card-text">{{ $template}}</pre>
         </div>
-        @endif
-    </form>
-</div>
+        <div class="card-footer">
+            <form role="form" method="POST" action="{{ route('labels.print', $templateName) }}">
+                {{ csrf_field() }} @if ( ! empty($fields))
+                <p>This template requires the following information to print.</p>
+                @foreach ($fields as $field)
+                <div class="form-group">
+                    <label for="{{ $field }}" class="form-label">{{ $field }}</label>
+                    <input class="form-control" id="{{ $field }}" type="text" name="{{ $field }}" value="{{ old($field, '') }}" required autofocus> @if ($errors->has($field))
+                    <p class="help-text">
+                        <strong>{{ $errors->first($field) }}</strong>
+                    </p>
+                    @endif
+                </div>
+                @endforeach @endif @if (SiteVisitor::inTheSpace())
+                <p>How many copies would you like.</p>
+                <hr>
+                <div class="form-group">
+                    <label for="copiesToPrint" class="form-label">Copies</label>
+                    <input class="form-control" style="width: 5%" id="copiesToPrint" type="text" name="copiesToPrint" value="{{ old('copiesToPrint', '1') }}" required autofocus> @if ($errors->has('copiesToPrint'))
+                    <p class="help-text">
+                        <strong>{{ $errors->first('copiesToPrint') }}</strong>
+                    </p>
+                    @endif
+                </div>
+                <hr>
+                <div class="card">
+                    <button type="submit" class="btn btn-success">Print</button>
+                </div>
+                @else
+                <div class="row">
+                    <p>Labels can only be printed from within the space.</p>
+                </div>
+                @endif
+            </form>
+        </div>
     </div>
 </div>
 
--- resources/views/labelTemplates/show.blade.php
+++ resources/views/labelTemplates/show.blade.php
@@ -1,32 +1,22 @@
-@extends('layouts.app')
-
-@section('pageTitle', 'Edit Label Template')
-
-@section('content')
+@extends('layouts.app') @section('pageTitle', 'Edit Label Template') @section('content')
 <div class="container">
-<div class="card">
-  <h4 class="card-header"> {{ $templateName}} </h4>
-  <div class="card-body">
-    <pre class="card-text">{{ $template}}</pre>
+    <div class="card">
+        <h4 class="card-header"> {{ $templateName}} </h4>
+        <div class="card-body">
+            <pre class="card-text">{{ $template}}</pre>
+        </div>
+        <div class="card-footer">
+            @can('labelTemplate.edit')
+            <a class="btn btn-primary btn-sm btn-sm-spacing" href="{{ route('labels.edit', $templateName) }}" class="button">Edit</a>
+            <a class="btn btn-primary btn-sm btn-sm-spacing" href="javascript:void(0);" onclick="$(this).find('form').submit();" class="alert button">
+                <form action="{{ route('labels.destroy', $templateName) }}" method="POST" style="display: inline">
+                    {{ method_field('DELETE') }} {{ csrf_field() }} Remove
+                </form>
+            </a>
+            @endcan @if (SiteVisitor::inTheSpace()) @can('labelTemplate.print')
+            <a class="btn btn-primary btn-sm btn-sm-spacing" href="{{ route('labels.showPrint', $templateName) }}" class="button">Print</a> @endcan @endif
+        </div>
     </div>
-    <div class="card-footer">
-    @can('labelTemplate.edit')
-     <a class="btn btn-primary btn-sm btn-sm-spacing" href="{{ route('labels.edit', $templateName) }}" class="button">Edit</a>
-      <a class="btn btn-primary btn-sm btn-sm-spacing" href="javascript:void(0);" onclick="$(this).find('form').submit();" class="alert button">
-        <form action="{{ route('labels.destroy', $templateName) }}" method="POST" style="display: inline">
-         {{ method_field('DELETE') }}
-         {{ csrf_field() }}
-         Remove
-        </form>
-        </a>
-@endcan
-@if (SiteVisitor::inTheSpace())
-    @can('labelTemplate.print')
-      <a class="btn btn-primary btn-sm btn-sm-spacing" href="{{ route('labels.showPrint', $templateName) }}" class="button">Print</a>
-    @endcan
-@endif
-  </div>
+
 </div>
-    
-    </div>
 @endsection

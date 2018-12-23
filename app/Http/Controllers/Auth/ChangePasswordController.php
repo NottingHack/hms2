bcac2b40981811e16131254d7d70c3319c559aae
dps.lwk <dps.lwk@gmail.com>
2018-12-23 10:03:39 +0000

    ChangePassword: docblock

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -37,7 +37,6 @@ class ChangePasswordController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)

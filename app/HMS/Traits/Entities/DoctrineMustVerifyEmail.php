c407ee8878f0330ed46ec7624dcd44e8c9c50e78
dpslwk <dps.lwk@gmail.com>
2018-12-23 18:49:40 +0000

    EmailValidation: Add email validation to Users

---
+++ app/HMS/Traits/Entities/DoctrineMustVerifyEmail.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace HMS\Traits\Entities;
+
+use Carbon\Carbon;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use LaravelDoctrine\ORM\Facades\EntityManager;
+
+trait DoctrineMustVerifyEmail
+{
+    /**
+     * @var null|Carbon Has the email address be verified
+     */
+    protected $emailVerifiedAt;
+
+    /**
+     * Determine if the user has verified their email address.
+     *
+     * @return bool
+     */
+    public function hasVerifiedEmail()
+    {
+        return ! is_null($this->emailVerifiedAt);
+    }
+
+    /**
+     * Mark the given user's email as verified.
+     *
+     * @return bool
+     */
+    public function markEmailAsVerified()
+    {
+        $repository = EntityManager::getRepository(get_class($this));
+        $this->emailVerifiedAt = new Carbon;
+        return $repository->save($this);
+    }
+
+    /**
+     * Send the email verification notification.
+     *
+     * @return void
+     */
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmail);
+    }
+
+    /**
+     * @return null|Carbon Has the email address be verified
+     */
+    public function getEmailVerifiedAt()
+    {
+        return $this->emailVerifiedAt;
+    }
+
+    /**
+     * @param null|Carbon Has the email address be verified $emailVerifiedAt
+     *
+     * @return self
+     */
+    public function setEmailVerifiedAt($emailVerifiedAt)
+    {
+        $this->emailVerifiedAt = $emailVerifiedAt;
+
+        return $this;
+    }
+}

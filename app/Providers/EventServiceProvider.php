0c083c3fdd496fcec2eb95fdd68e06e3bdbef9ad
dpslwk <dps.lwk@gmail.com>
2018-12-23 10:39:06 +0000

    EmailValidation: Laravel 5.7 missing listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -14,6 +15,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         'Illuminate\Auth\Events\Registered' => [
+            SendEmailVerificationNotification::class,
             'App\Listeners\Invites\RevokeInviteOnUserRegistered',
             'App\Listeners\Membership\ApprovalEmailOnUserRegistered',
         ],

d17f117be5b10ae6589ce6b7b50e819604d0e7bd
dpslwk <dps.lwk@gmail.com>
2018-12-31 19:40:31 +0000

    Frontend: Split out the app.blade to header, nav, footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,55 @@
+<!-- footer -->
+<footer>
+  <div class="container">
+    <div class="footer">
+      <div class="col-sm-3">
+        <h5>HMS 2.0.0</h5>
+        <ul>
+          <li><a href="#">Get Source</a></li>
+          <li><a href="#">Credits</a></li>
+          <li><a href="#">Nottinghack Website</a></li>
+          <li>© {{ \Carbon\Carbon::now()->year }} Nottinghack</li>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <h5>About us</h5>
+        <ul>
+          <li><a href="#">Company Information</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">Reviews</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <h5>Address</h5>
+        <ul>
+          <li>Unit F6 Bizspace</li>
+          <li>Roden House</li>
+          <li>Business Centre</li>
+          <li>Nottingham</li>
+          <li>NG3 1JH</li>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <h5>Legal</h5>
+        <ul>
+          <li>Nottingham Hackspace Ltd</li>
+          <li>No. 07766826</li>
+          <li>Reg. in England & Wales</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="container">
+    <div class="footer">
+      <div class="social-networks">
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="facebook"><i class="fa fa-facebook-official"></i></a>
+        <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+        <a href="#" class="flickr"><i class="fa fa-flickr"></i></a>
+        <a href="#" class="youtube"><i class="fa fa-youtube"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- footer end -->
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,47 @@
+<!-- navbar -->
+<nav class="userbar">
+  @if (!Auth::guest() and isset($mainNav) )
+  {{-- build the nav toggler --}}
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa fa-bars"></i>&nbsp;Menu
+  </button>
+  {{-- build the nav --}}
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      @foreach ($mainNav as $link)
+      @if (count($link['links']) > 0)
+      <li class="nav-item {!! $link['active'] ? 'active' : '' !!} dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopups="true" aria-expanded="false" href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+        <div class="dropdown-menu">
+          @foreach ($link['links'] as $subLink)
+          <a class="dropdown-item" href="{{ $subLink['url'] }}">{{ $subLink['text'] }}</a>
+          @endforeach
+        </div>
+      </li>
+      @else
+      <li class="nav-item {!! $link['active'] ? 'active' : '' !!}">
+        <a class="nav-link" href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+      </li>
+      @endif
+      @endforeach
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="navbar-text ml-auto">Logged in as {{ Auth::user()->getFirstName() }} @if (Auth::viaRemember()) (via Remember Me) @endif</li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a>
+        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+          {{ csrf_field() }}
+        </form>
+      </li>
+    </ul>
+  </div>
+  @elseif (Auth::guest())
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Log In</a></li>
+    @if (SiteVisitor::inTheSpace())
+    <li class="nav-item"><a class="nav-link" href="{{ route('registerInterest') }}">Register Interest</a></li>
+    @endif
+  </ul>
+  @endif
+</nav>
+<!-- navbar end -->

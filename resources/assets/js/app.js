2e6798b49fb76e1eea7ccebbfa4142811778a47f
dpslwk <dps.lwk@gmail.com>
2018-12-31 14:22:23 +0000

    Mix: Update to laravel-mix 4 and forget browserSync() as its not going to work

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,8 @@ require('./bootstrap');
 // This file is for global JavaScript which is to be present on every page.
 // It is compiled by Webpack, so can freely contain ES2015 code.
 
+window.select2 = require('select2')
+
 // Negative money values should be red
 $(".money:contains('-')").css("color", "red");
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,12 +14,8 @@ const mix = require('laravel-mix');
 mix.webpackConfig({
         devtool: 'source-map'
     })
-   .autoload({
-        jquery: ['jQuery', '$', 'window.jQuery'],
-        'popper.js/dist/umd/popper.js': ['Popper']
-    })
    .js('resources/assets/js/app.js', 'public/js')
-   .extract(['lodash', 'jquery', 'axios', 'select2', 'bootstrap', 'popper.js'])
+   .extract()
    .sass('resources/assets/sass/app.scss', 'public/css')
    .version()
-   .browserSync('hmsdev');
+   .sourceMaps();

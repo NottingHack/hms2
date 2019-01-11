0fdbfa162051da84505dc32b35aebe3f00335efb
dpslwk <dps.lwk@gmail.com>
2019-01-11 04:20:43 +0000

    Entities: add Instrumentation LightLevel

---
+++ app/HMS/Entities/Instrumentation/LightLevel.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace HMS\Entities\Instrumentation;
+
+use Carbon\Carbon;
+
+class LightLevel
+{
+    /**
+     * @var string
+     */
+    protected $sensor;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var int
+     */
+    protected $reading;
+
+    /**
+     * @var Carbon
+     */
+    protected $time;
+
+    /**
+     * @return string
+     */
+    public function getSensor()
+    {
+        return $this->sensor;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getReading()
+    {
+        return $this->reading;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+}
---
+++ database/migrations_doctrine/Version20190111041806_add_instrumenation_light_level_table.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20190111041806_add_instrumenation_light_level_table extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE light_level (sensor VARCHAR(30) NOT NULL, name VARCHAR(100) DEFAULT NULL, reading INT DEFAULT NULL, `time` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(sensor)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE light_level');
+    }
+}

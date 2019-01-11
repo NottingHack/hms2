f8b90733172864cc4e4aa7a4501fac9614245cb5
dpslwk <dps.lwk@gmail.com>
2019-01-11 05:33:52 +0000

    Entities: add Instrumentation Service

---
+++ app/HMS/Entities/Instrumentation/Service.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace HMS\Entities\Instrumentation;
+
+use Carbon\Carbon;
+
+class Service
+{
+    /**
+     * Primary key
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var int
+     */
+    protected $status;
+
+    /**
+     * @var string
+     */
+    protected $statusString;
+
+    /**
+     * @var Carbon
+     */
+    protected $queryTime;
+
+    /**
+     * @var Carbon
+     */
+    protected $replyTime;
+
+    /**
+     * @var Carbon
+     */
+    protected $restartTime;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * Gets the value of id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatusString()
+    {
+        return $this->statusString;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getQueryTime()
+    {
+        return $this->queryTime;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getReplyTime()
+    {
+        return $this->replyTime;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getRestartTime()
+    {
+        return $this->restartTime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
---
+++ database/migrations_doctrine/Version20190111050458_add_instrumentaion_service_table.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20190111050458_add_instrumentaion_service_table extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE service_status (service_name VARCHAR(100) NOT NULL, status INT NOT NULL, status_str VARCHAR(255) DEFAULT NULL, query_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, reply_time DATETIME DEFAULT NULL, restart_time DATETIME DEFAULT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(service_name)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE service_status');
+    }
+}

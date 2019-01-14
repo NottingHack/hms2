f2d26ec6556aeee3b9a34eec8f062835ac677927
dpslwk <dps.lwk@gmail.com>
2019-01-14 17:35:56 +0000

    Bookings: import from google calendar api script

---
+++ dev/googleCalendarImport/migrate.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = new LWK\GoogleCalendarImport\MigrateGoogle(
+    realpath(__DIR__)
+);
+
+$status = $app->handle();
+
+exit($status);

d5288503c810eed8596db5cfc86b99c51d4a67c5
dpslwk <dps.lwk@gmail.com>
2019-05-24 04:51:02 +0100

    Gate: add CanOr & CanAnd gates

---
+++ app/Http/Middleware/AuthorizeOr.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Access\Gate;
+
+class AuthorizeOr
+{
+    /**
+     * The gate instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Access\Gate
+     */
+    protected $gate;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param \Illuminate\Contracts\Auth\Access\Gate  $gate
+     *
+     * @return void
+     */
+    public function __construct(Gate $gate)
+    {
+        $this->gate = $gate;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @param \Closure  $next
+     * @param string  ...$abilities
+     *
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function handle($request, Closure $next, ...$abilities)
+    {
+        $this->gate->any($abilities);
+
+        return $next($request);
+    }
+}

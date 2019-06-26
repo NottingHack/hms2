1c1ba08fdb60e2104929dfd84e960028848706e2
dpslwk <dps.lwk@gmail.com>
2019-06-26 20:57:43 +0100

    Gatekeeper: rework migration

--- database/migrations_doctrine/Version20190626181021_alter_zone_id_to_signed.php
+++ database/migrations_doctrine/Version20190626181021_alter_zone_id_to_signed.php
@@ -16,14 +16,25 @@ class Version20190626181021_alter_zone_id_to_signed extends AbstractMigration
 
         $this->addSql('SET foreign_key_checks = 0');
 
+        $this->addSql('ALTER TABLE doors DROP FOREIGN KEY FK_5E5B762A49451B7F');
+        $this->addSql('ALTER TABLE doors DROP FOREIGN KEY FK_5E5B762A70C827BA');
+        $this->addSql('ALTER TABLE zone_occupancy_logs DROP FOREIGN KEY FK_59ADE3689F2C3FAB');
+        $this->addSql('ALTER TABLE zone_occupants DROP FOREIGN KEY FK_EECE6E0B9F2C3FAB');
+
         $this->addSql('ALTER TABLE zone_occupancy_logs CHANGE zone_id zone_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE zones CHANGE id id INT AUTO_INCREMENT NOT NULL');
         $this->addSql('ALTER TABLE zone_occupants CHANGE zone_id zone_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE doors CHANGE side_a_zone_id side_a_zone_id INT DEFAULT NULL, CHANGE side_b_zone_id side_b_zone_id INT DEFAULT NULL');
 
-        // shift zone id's down by one
+        $this->addSql('ALTER TABLE zone_occupants ADD CONSTRAINT FK_EECE6E0B9F2C3FAB FOREIGN KEY (zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE zone_occupancy_logs ADD CONSTRAINT FK_59ADE3689F2C3FAB FOREIGN KEY (zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE doors ADD CONSTRAINT FK_5E5B762A70C827BA FOREIGN KEY (side_a_zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE doors ADD CONSTRAINT FK_5E5B762A49451B7F FOREIGN KEY (side_b_zone_id) REFERENCES zones (id)');
 
+        // shift zone id's down by one
         $this->addSql('UPDATE zones SET id = id-1');
+        $this->addSql('UPDATE zone_occupancy_logs SET zone_id = zone_id-1');
+        $this->addSql('UPDATE zone_occupants SET zone_id = zone_id-1');
         $this->addSql('UPDATE doors SET side_a_zone_id = side_a_zone_id-1 WHERE side_a_zone_id IS NOT NULL');
         $this->addSql('UPDATE doors SET side_b_zone_id = side_b_zone_id-1 WHERE side_b_zone_id IS NOT NULL');
 
@@ -39,14 +50,28 @@ class Version20190626181021_alter_zone_id_to_signed extends AbstractMigration
 
         $this->addSql('SET foreign_key_checks = 0');
 
-        $this->addSql('UPDATE zones SET id = id+1 ORDER BY ID DESC');
+        // shift zone id's down by one
+        $this->addSql('UPDATE zones SET id = id+1');
+        $this->addSql('UPDATE zone_occupancy_logs SET zone_id = zone_id+1');
+        $this->addSql('UPDATE zone_occupants SET zone_id = zone_id+1');
         $this->addSql('UPDATE doors SET side_a_zone_id = side_a_zone_id+1 WHERE side_a_zone_id IS NOT NULL');
         $this->addSql('UPDATE doors SET side_b_zone_id = side_b_zone_id+1 WHERE side_b_zone_id IS NOT NULL');
+
+        $this->addSql('ALTER TABLE doors DROP FOREIGN KEY FK_5E5B762A49451B7F');
+        $this->addSql('ALTER TABLE doors DROP FOREIGN KEY FK_5E5B762A70C827BA');
+        $this->addSql('ALTER TABLE zone_occupancy_logs DROP FOREIGN KEY FK_59ADE3689F2C3FAB');
+        $this->addSql('ALTER TABLE zone_occupants DROP FOREIGN KEY FK_EECE6E0B9F2C3FAB');
 
         $this->addSql('ALTER TABLE doors CHANGE side_a_zone_id side_a_zone_id INT UNSIGNED DEFAULT NULL, CHANGE side_b_zone_id side_b_zone_id INT UNSIGNED DEFAULT NULL');
         $this->addSql('ALTER TABLE zone_occupancy_logs CHANGE zone_id zone_id INT UNSIGNED DEFAULT NULL');
         $this->addSql('ALTER TABLE zone_occupants CHANGE zone_id zone_id INT UNSIGNED DEFAULT NULL');
         $this->addSql('ALTER TABLE zones CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL');
+
+        $this->addSql('ALTER TABLE zone_occupants ADD CONSTRAINT FK_EECE6E0B9F2C3FAB FOREIGN KEY (zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE zone_occupancy_logs ADD CONSTRAINT FK_59ADE3689F2C3FAB FOREIGN KEY (zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE doors ADD CONSTRAINT FK_5E5B762A70C827BA FOREIGN KEY (side_a_zone_id) REFERENCES zones (id)');
+        $this->addSql('ALTER TABLE doors ADD CONSTRAINT FK_5E5B762A49451B7F FOREIGN KEY (side_b_zone_id) REFERENCES zones (id)');
+
 
         $this->addSql('SET foreign_key_checks = 1');
     }

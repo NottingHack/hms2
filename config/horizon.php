e2224d75d7ebed92ddef03cb75d751af1542f7d5
dpslwk <dps.lwk@gmail.com>
2019-06-29 18:10:50 +0100

    PrintLabel: change handlePrint to printEvent fixes #417
    event auto-discover looks for any method starting with `handle`
    so that and  `EventServiceProvider $subscribe` were both registering this listener and it was getting queued twice

--- config/horizon.php
+++ config/horizon.php
@@ -152,7 +152,7 @@ return [
         'local' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue' => ['default', 'maintanance'],
+                'queue' => ['default', 'maintenance'],
                 'balance' => 'simple',
                 'processes' => 3,
                 'tries' => 3,
@@ -162,7 +162,7 @@ return [
         'rommie' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue' => ['default', 'maintanance'],
+                'queue' => ['default', 'maintenance'],
                 'balance' => 'simple',
                 'processes' => 3,
                 'tries' => 3,

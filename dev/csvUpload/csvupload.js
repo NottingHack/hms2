8547f16e26f91d81745ff61b2861b0ac55b878fb
dpslwk <dps.lwk@gmail.com>
2019-07-07 20:25:03 +0100

    CSVUploadTool: borrowing from TSBscrape to finish off

--- dev/csvUpload/csvupload.js
+++ dev/csvUpload/csvupload.js
@@ -2,10 +2,10 @@
 const path = require('path');
 const fs = require('fs');
 const csv = require('csvtojson')
-const https = require('https');
-const request = require('request');
-
 const program = require('commander');
+const axios = require('axios');
+const oauth = require('axios-oauth-client');
+const tokenProvider = require('axios-token-interceptor');
 
 const pkg = require('./package.json');
 const cerfile = require('../nottinghack-ca.crt.pem');
@@ -14,96 +14,77 @@ const casigningcert = fs.readFileSync(cerfile)
 program
   .version(pkg.version)
   .description('Programmatic upload CSV to hms2.')
-  .option('-t, --token <token>', 'Client token');
-
-program
-  .command('upload_csv <in_csv>')
-  .description('Upload given csv file')
-  .action(function (in_csv, options) {
-    if (!program.token) {
-        console.error('--token must be specified');
-    }
-
-    csv()
-    .fromFile(in_csv)
-    .then((jsonObj)=>{
-        transactions = jsonObj.map(function (transaction) {
-            if (transaction['Debit Amount'] != '') {
-                amount = '-' . transaction['Debit Amount'];
-            } else {
-                amount = transaction['Credit Amount'];
-            } 
-
-            const dateParts = transaction['Transaction Date'].split('/');
-            const formatedDate = dateParts.reverse().join('-');
-            
-            return {
-                'sortCode' : transaction['Sort Code'].replace('\'', ''),
-                'accountNumber' : transaction['Account Number'],
-                'date' : formatedDate,
-                'description' : transaction['Transaction Description'],
-                'amount' : amount
-                }
-        });
-        uplaodJsonTransactions(transactions);
-    })
-
+  .arguments('<client_id> <client_secret> <in_csv>')
+  .action(function (client_id, client_secret) {
+     clientIdValue = client_id;
+     clientSecretValue = client_secret;
+     inCSVValue = in_csv;
   });
 
 program.parse(process.argv);
 
+if (typeof clientIdValue === 'undefined') {
+   console.error('no client ID given!');
+   process.exit(1);
+}
+if (typeof clientSecretValue === 'undefined') {
+   console.error('no client Secret given!');
+   process.exit(1);
+}
+if (typeof inCSVValue === 'undefined') {
+   console.error('no CSV given!');
+   process.exit(1);
+}
+
+csv()
+.fromFile(inCSVValue)
+.then((jsonObj)=>{
+    transactions = jsonObj.map(function (transaction) {
+        if (transaction['Debit Amount'] != '') {
+            amount = '-' . transaction['Debit Amount'];
+        } else {
+            amount = transaction['Credit Amount'];
+        }
+
+        const txnDate = new Date(transaction['Transaction Date']. + ' UTC');
+
+        return {
+            'sortCode' : transaction['Sort Code'].replace('\'', ''),
+            'accountNumber' : transaction['Account Number'],
+            'date' : txnDate->toJSON(),
+            'description' : transaction['Transaction Description'],
+            'amount' : amount
+            }
+    });
+    uplaodJsonTransactions(transactions);
+});
+
 function uplaodJsonTransactions(transactions)
 {
-    const data = JSON.stringify(transactions);
+  console.log(transactions);
+  program.exit(0);
+  // setup oauth and axios
+  const getOwnerCredentials = oauth.client(axios.create(), {
+    url: 'https://hmsdev/oauth/token',
+    grant_type: 'client_credentials',
+    client_id: clientIdValue,
+    client_secret: clientSecretValue,
+  });
 
-    let options = { method: 'POST',
-      url: 'https://hmsdev/oauth/token',
-      headers: { 'content-type': 'application/json' },
-      body: 
-       { grant_type: 'client_credentials',
-         client_id: '3',
-         client_secret: 'zYVY1rmybiKrrBMjam3e6d3kR13jv51tXopG83Iq' },
-      json: true,
-      ca: casigningcert 
-    };
+  const instance = axios.create();
+  instance.interceptors.request.use(
+    // Wraps axios-token-interceptor with oauth-specific configuration,
+    // fetches the token using the desired claim method, and caches
+    // until the token expires
+    oauth.interceptor(tokenProvider, getOwnerCredentials)
+  );
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-
-      const json = JSON.parse(body);
-
-      // store the token
-      console.log("Access Token:", json.access_token);
-    });
-
-    // const options = {
-    //   hostname: 'hmsdev',
-    //   port: 443,
-    //   path: '/api/bank_transactions/upload',
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Content-Length': data.length,
-    //     'Authorization': 'Bearer '+program.token
-    //   },
-    //   ca: casigningcert
-    // };
-
-    // console.log(options);
-
-    // const req = https.request(options, (res) => {
-    //   console.log(`statusCode: ${res.statusCode}`)
-
-    //   res.on('data', (d) => {
-    //     process.stdout.write(d)
-    //   })
-    // })
-
-    // req.on('error', (error) => {
-    //   console.error(error)
-    // })
-
-    // req.write(data)
-    // req.end()
-
+  instance.post('https://hmsdev/api/bank_transactions/upload', transactions)
+  .then(function (response) {
+    console.log('Transactions Uploaded');
+    // console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }

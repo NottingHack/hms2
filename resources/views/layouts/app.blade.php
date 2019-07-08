36605782358583d071313731ec884d20329a5510
dpslwk <dps.lwk@gmail.com>
2019-07-08 11:44:13 +0100

    Favicon: all change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,15 +19,20 @@
   <script src="{{ mix('/js/app.js') }}" defer></script>
 
   <!-- fav icons -->
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+  <link rel="shortcut icon" href="/favicon.ico?v=XBJgQp70gw" type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
   <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
   <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
   <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=XBJgQp70gw">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=XBJgQp70gw">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=XBJgQp70gw">
+  <link rel="manifest" href="/site.webmanifest?v=XBJgQp70gw">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg?v=XBJgQp70gw" color="#195905">
+  <meta name="apple-mobile-web-app-title" content="HMS">
+  <meta name="application-name" content="HMS">
+  <meta name="msapplication-TileColor" content="#195905">
+  <meta name="theme-color" content="#ffffff">
   <!-- head end -->
 </head>
 

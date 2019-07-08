976356a3ae239e2439df11de724db09844240342
dpslwk <dps.lwk@gmail.com>
2019-07-08 05:49:12 +0100

    Bookings: add type key

--- resources/views/tools/booking/index.blade.php
+++ resources/views/tools/booking/index.blade.php
@@ -4,9 +4,10 @@
 
 @section('content')
 <div class="container">
-<p>To add a booking, select and drag on the calendar below.</p>
-<p>To re-schedule the booking, click and move the booking to a new time.</p>
-<p>To cancel a booking, click on the booking then confirm cancellation. </p>
+  <p>To add a booking, select and drag on the calendar below.</p>
+  <p>To re-schedule the booking, click and move the booking to a new time.</p>
+  <p>To cancel a booking, click on the booking then confirm cancellation.</p>
+  <p>Each type of booking is represented by a different colour.<br><span class="btn btn-booking-normal btn-sm mb-1">Normal</span> <span class="btn btn-booking-induction btn-sm mb-1">Induction</span> <spann class="btn btn-booking-maintenance btn-sm mb-1">Maintenance</spann></p>
 </div>
 
 <tool-calendar

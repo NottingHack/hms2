98e8911d07e1079ecf2984d78bce45912b12d463
dpslwk <dps.lwk@gmail.com>
2019-07-19 08:30:45 +0100

    Team: queue reminder email

--- app/Mail/Teams/TeamReminder.php
+++ app/Mail/Teams/TeamReminder.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class TeamReminder extends Mailable
+class TeamReminder extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 

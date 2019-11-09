1912b0acedfbd716637ae76f8a35b1ff584418af
dpslwk <dps.lwk@gmail.com>
2019-11-09 05:41:16 +0000

    Stats: Adding Members, Snackspace and Zone Occupancy stats

---
+++ app/Http/Controllers/StatisticsController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use HMS\Views\MemberStats;
+use Illuminate\Http\Request;
+use HMS\Views\SnackspaceMonthly;
+use Illuminate\Support\Facades\Cache;
+use HMS\Repositories\GateKeeper\ZoneRepository;
+
+class StatisticsController extends Controller
+{
+    /**
+     * @var ZoneRepository
+     */
+    protected $zoneRepository;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param ZoneRepository $zoneRepository
+     */
+    public function __construct(ZoneRepository $zoneRepository)
+    {
+        $this->zoneRepository = $zoneRepository;
+    }
+
+    /**
+     * Show membership statistics view.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function memberStats()
+    {
+        // pull stats from the cache if they are there, if not store a fresh copy for 1 day
+        $memberStats = Cache::remember('statistics.membership', 86400, function () {
+            return \HMS\Views\MemberStats::first();
+        });
+
+        return view('statistics.member_stats')
+            ->with('memberStats', $memberStats);
+    }
+
+    /**
+     * Show membership statistics view.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function snackspaceMonthly()
+    {
+        // pull stats from the cache if they are there, if not store a fresh copy for 1 day
+        $snackspaceMonthly = Cache::remember('statistics.snackspace-monthly', 86400, function () {
+            config(['database.connections.mysql.strict' => false]);
+            \DB::reconnect(); //important as the existing connection if any would be in strict mode
+            $snackspaceMonthly = SnackspaceMonthly::all();
+
+            //now changing back the strict ON
+            config(['database.connections.mysql.strict' => true]);
+            \DB::reconnect();
+            return $snackspaceMonthly;
+        });
+
+        return view('statistics.snackspace_monthly')
+            ->with('snackspaceMonthly', $snackspaceMonthly);
+    }
+
+    /**
+     * Show zone occupancy view.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function zoneOccupancy()
+    {
+        $zones = $this->zoneRepository->findAll();
+
+        return view('statistics.zone_occupants')
+            ->with('zones', $zones);
+    }
+}
---
+++ resources/views/statistics/zone_occupants.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Membership Statistics')
+
+@section('content')
+<div class="container">
+  <p>This shows the number of current members in each area of the hackspace</p>
+
+  <p>These are very rough numbers based on RFID entries and exits where available. We don't track exits at all doors and tailgating can no be account for. Every 12 hours we reset any members in a zone for over 24 hours back to off-site.</p>
+  <table class="table">
+    <thead>
+      <th>Zone</th>
+      <th>Occupants</th>
+    </thead>
+    <tbody>
+      @foreach($zones as $zone)
+      <tr>
+        <td>{{ $zone->getDescription() }}</td>
+        <td>{{ count($zone->getZoneOccupancts()) }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+@endsection

1d1548034e4ca39f66842743977ebd96016569b9
dpslwk <dps.lwk@gmail.com>
2020-01-05 02:01:39 +0000

    StyleCI

--- database/migrations_doctrine/Version20200104232350_membership_rejected_logs.php
+++ database/migrations_doctrine/Version20200104232350_membership_rejected_logs.php
@@ -2,8 +2,8 @@
 
 namespace Database\Migrations;
 
-use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 class Version20200104232350_membership_rejected_logs extends AbstractMigration
 {

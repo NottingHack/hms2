f47a2bc235bd6c9e9e29f7512bebd39b4cbff18e
dpslwk <dps.lwk@gmail.com>
2020-05-15 15:29:33 +0100

    Correct some state column lengths
    cause i can spell

---
+++ database/migrations_doctrine/Version20200515144036_alter_state_column_lenths.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20200515144036_alter_state_column_lenths extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE membership_status_notifications CHANGE cleared_reason cleared_reason VARCHAR(20) DEFAULT NULL');
+        $this->addSql('ALTER TABLE bookings CHANGE type type VARCHAR(20) NOT NULL');
+        $this->addSql('ALTER TABLE doors CHANGE state state VARCHAR(20) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE bookings CHANGE type type VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE doors CHANGE state state VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE membership_status_notifications CHANGE cleared_reason cleared_reason VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}

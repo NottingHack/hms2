bc624fbb07f7fe3a51626838042caac061b9184e
dpslwk <dps.lwk@gmail.com>
2020-06-22 16:44:32 +0100

    TemporaryAcccess Jobs: woops

--- app/Jobs/Gatekeeper/UpdateTemporaryAccessRoleJob.php
+++ app/Jobs/Gatekeeper/UpdateTemporaryAccessRoleJob.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use HMS\Entities\Role;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Log;
+use HMS\Repositories\MetaRepository;
 use HMS\Repositories\RoleRepository;
 use HMS\User\Permissions\RoleManager;
 use Illuminate\Queue\SerializesModels;

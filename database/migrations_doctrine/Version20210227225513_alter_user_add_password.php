525d3f246ca49560f8ad9fdac0d9cffe59af9a34
dpslwk <dps.lwk@gmail.com>
2021-02-27 23:50:53 +0000

    User: add password field for use with DoctrinePasswordStore

---
+++ database/migrations_doctrine/Version20210227225513_alter_user_add_password.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20210227225513_alter_user_add_password extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD password VARCHAR(255) NOT NULL AFTER email');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP password');
+    }
+}

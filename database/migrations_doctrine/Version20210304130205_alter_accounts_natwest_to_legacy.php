8faa8dd8ac27876b83b00a058813476580dbab3d
dpslwk <dps.lwk@gmail.com>
2021-03-04 13:44:22 +0000

    Account: natwestRef -> legacyRef
    missed the migration

---
+++ database/migrations_doctrine/Version20210304130205_alter_accounts_natwest_to_legacy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20210304130205_alter_accounts_natwest_to_legacy extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE accounts CHANGE natwest_ref legacy_ref VARCHAR(18) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE accounts CHANGE legacy_ref natwest_ref VARCHAR(18) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}

ab8677dd3f41a59fd510b681f7fd7b2e72b4e7eb
dpslwk <dps.lwk@gmail.com>
2021-03-04 12:45:33 +0000

    BankTransactions: fix uniqueConstraints to include bank_id

---
+++ database/migrations_doctrine/Version20210304123938_alter_bank_transactions_duplicate_idx.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema as Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20210304123938_alter_bank_transactions_duplicate_idx extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX duplicate_idx ON bank_transactions');
+        $this->addSql('CREATE UNIQUE INDEX duplicate_idx ON bank_transactions (bank_id, transaction_date, description, amount)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX duplicate_idx ON bank_transactions');
+        $this->addSql('CREATE UNIQUE INDEX duplicate_idx ON bank_transactions (transaction_date, description, amount)');
+    }
+}

dffd31390ee871fedfde1c2d1df8cf7693033ee3
dpslwk <dps.lwk@gmail.com>
2023-05-07 18:23:39 +0100

    RecalculateMeetingQuorumJob: missed import

--- app/Jobs/Governance/RecalculateMeetingQuorumJob.php
+++ app/Jobs/Governance/RecalculateMeetingQuorumJob.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Laravel\Horizon\Contracts\Silenced;
 
 class RecalculateMeetingQuorumJob implements ShouldQueue, Silenced
 {

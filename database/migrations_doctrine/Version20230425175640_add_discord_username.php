c50101da7fb29ff19a465acad624681df6a1e654
Aaron Jackson <aaron@aaronsplace.co.uk>
2023-05-07 22:26:34 +0100

    Manually remove changes to job_batches
    I'm not sure why but it's trying to drop job_batches, even though I
    ran `php artisan migrate:refresh`

--- database/migrations_doctrine/Version20230425175640_add_discord_username.php
+++ database/migrations_doctrine/Version20230425175640_add_discord_username.php
@@ -14,7 +14,6 @@ class Version20230425175640_add_discord_username extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE job_batches');
         $this->addSql('ALTER TABLE profile ADD discord_id VARCHAR(32) DEFAULT NULL');
     }
 
@@ -25,7 +24,6 @@ class Version20230425175640_add_discord_username extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE job_batches (id VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, total_jobs INT NOT NULL, pending_jobs INT NOT NULL, failed_jobs INT NOT NULL, failed_job_ids TEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, options MEDIUMTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, cancelled_at INT DEFAULT NULL, created_at INT NOT NULL, finished_at INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE profile DROP discord_id');
     }
 }

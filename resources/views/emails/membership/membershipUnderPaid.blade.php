1e797e81a4bcb00604645ccb06baa7770ad1b70b
Aaron Jackson <aaron@aaronsplace.co.uk>
2023-08-20 16:25:07 +0100

    minor correction from steve

--- resources/views/emails/membership/membershipUnderPaid.blade.php
+++ resources/views/emails/membership/membershipUnderPaid.blade.php
@@ -4,7 +4,7 @@
 @content('emails.membership.membershipUnderPaid', 'main')
 
 
-Thank you for your membership payment to {{ config('branding.space_name') }}. Unfortunately your payment must be at least **@money($minimumAmount, 'GBP')** for membership. In order to complete you membership, please make a payment of @money($minimumAmount, 'GBP') or more, within the next few days, using your payment reference.
+Thank you for your membership payment to {{ config('branding.space_name') }}. Unfortunately your payment must be at least **@money($minimumAmount, 'GBP')** for membership. In order to complete your membership, please make a payment of @money($minimumAmount, 'GBP') or more, within the next few days, using your payment reference.
 
 Any amount below the minimum is considered a donation.
 

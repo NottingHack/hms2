c5cb0d5b84e02192cb571c7156b5f02f01396efe
Alistair Buxton <a.j.buxton@gmail.com>
2025-04-17 15:54:50 +0100

    Fix some typos on the boxes and zones status pages

--- resources/views/statistics/zone_occupants.blade.php
+++ resources/views/statistics/zone_occupants.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
   <p>This shows the number of current members in each area of the {{ config('branding.space_type') }}</p>
 
-  <p>These are very rough numbers based on RFID entries and exits where available. We don't track exits at all doors and tailgating can no be account for. Every 12 hours we reset any members in a zone for over {{ $resetHours }} hours back to off-site.</p>
+  <p>These are very rough numbers based on RFID entries and exits where available. We don't track exits at all doors and tailgating can not be accounted for. Every 12 hours we reset any members in a zone for over {{ $resetHours }} hours back to off-site.</p>
   <table class="table">
     <thead>
       <th>Zone</th>

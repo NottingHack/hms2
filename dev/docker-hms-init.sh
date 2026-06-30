b4cebb63a4592b16a0392fba63fa280c79b71b2b
Aaron Jackson <aaron@aaronsplace.co.uk>
2026-06-30 17:47:22 +0100

    persist between up/down

--- dev/docker-hms-init.sh
+++ dev/docker-hms-init.sh
@@ -5,26 +5,27 @@ mkdir $HOME
 
 cd /hms
 
-composer upgrade --no-security-blocking
-
+# Stuff to happen on the first run only
 [ -f .env ] || (
     cp .env.example .env
     sed -i '/MAIL_HOST/cMAIL_HOST=hms-mailpit/' .env
     sed -i '/REDIS_HOST/cREDIS_HOST=hms-redis/' .env
     sed -i '/MEMCACHED_HOST/cMEMCACHED_HOST=hms-memcached/' .env
     php artisan key:generate
+
+    composer install --no-security-blocking
+
+    php artisan migrate
+    php artisan doctrine:migration:refresh -n
+    php artisan hms:database:refresh-views
+    php artisan hms:database:refresh-procedures
+    php artisan permissions:defaults
+    php artisan meta:sync
+    php artisan db:seed
+    yes | php artisan passport:install
+    yes | php artisan ziggy:generate
+
+    npm ci
+    npm run dev
 )
 
-php artisan key:generate
-php artisan migrate
-php artisan doctrine:migration:refresh -n
-php artisan hms:database:refresh-views
-php artisan hms:database:refresh-procedures
-php artisan permissions:defaults
-php artisan meta:sync
-php artisan db:seed
-yes | php artisan passport:install
-yes | php artisan ziggy:generate
-
-npm install
-npm run dev
